49612ca031fd43651da363db94bffd391f55f2af
sravya2324 <158995632+sravya2324@users.noreply.github.com>
2024-02-05 12:17:59 +0530

    Add files via upload

---
+++ springbootapplication-1/src/main/java/com/sathya/controller/Mycontroller.java
@@ -0,0 +1,15 @@
+package com.sathya.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class Mycontroller {
+@RequestMapping("/home")
+  public String homePage()
+  {
+	  return "home";
+  }
+  
+}
---
+++ springbootapplication-1/src/main/webapp/views/home.jsp
@@ -0,0 +1,1 @@
+<h1 style="color:green">welcome to springboot</h1>
